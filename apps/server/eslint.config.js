b25ed1357c83066ce750003ea4de59d2eaa08bd7
Semion Kiselev <semion.kiselev@gmail.com>
2024-11-26 22:40:34 +0300

    update eslint

--- apps/server/eslint.config.js
+++ apps/server/eslint.config.js
@@ -3,9 +3,17 @@ import pluginJs from "@eslint/js";
 import tsEslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
+console.log(pluginJs.configs.recommended)
+console.log(...tsEslint.configs.recommended)
+
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: globals.nodeBuiltin }},
   pluginJs.configs.recommended,
   ...tsEslint.configs.recommended,
   eslintConfigPrettier,
+  {
+    rules: {
+      ['@typescript-eslint/no-unused-vars']: 'warn'
+    }
+  }
 ];
